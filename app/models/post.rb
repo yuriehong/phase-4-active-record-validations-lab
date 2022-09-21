70d713f0c0cb49f8d15c102aa35104cfeafe5ddc
+ <+>
2022-09-20 21:31:47 -0400

    commit tree 00f65b3e0029a4700a9c842bb9f2876d12ca395a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction NonFiction)}
+
+
 end
